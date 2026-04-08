3d97de660b2f28979cc1f7cc2f04ca2355b54de7
paicodev <paedmaag@gmail.com>
2026-04-08 19:15:06 -0300

    init: Se creó el proyecto con los archivos iniciales

---
+++ Clase Estudiante/src/Estudiante.java
@@ -0,0 +1,62 @@
+public class Estudiante{
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String carrera;
+    private double promedio;
+
+    public Estudiante(){
+
+    }
+
+    public Estudiante(String nombre, String apellido, int edad, String carrera, double promedio){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.edad=edad;
+        this.carrera=carrera;
+        this.promedio=promedio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public Double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(Double promedio) {
+        this.promedio = promedio;
+    }
+
+    
+    
+}
