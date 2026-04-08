628c4386fa92e49ce8341947fb0f8f00ed5638ab
paicodev <paedmaag@gmail.com>
2026-04-08 19:21:30 -0300

    dev: Se agregó la impresión de los datos de los Estudiantes.
    -Se agregó un array de tipo Estudiantes para guardar a los objetos instanciados de la clase Estudiante.
    -Por medio de un bucle for se imprimieron los datos correspondientes a los alumnos en la posición determinada.

--- Clase Estudiante/src/App.java
+++ Clase Estudiante/src/App.java
@@ -1,9 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Estudiante estudiante1 = new Estudiante("Pablo", "Lopez", 23, "Programador Universitario en Sistemas", 9.1); 
-        Estudiante estudiante2 = new Estudiante("Fernando", "Torres", 23, "Programador Universitario en Sistemas", 8.1); 
-        Estudiante estudiante3 = new Estudiante("Esteban", "Mendez", 23, "Programador Universitario en Sistemas", 8.7);
+        Estudiante estudiante2 = new Estudiante("Fernando", "Torres", 24, "Programador Universitario en Sistemas", 8.1); 
+        Estudiante estudiante3 = new Estudiante("Esteban", "Mendez", 25, "Programador Universitario en Sistemas", 8.7);
         
-        
+        Estudiante listado[] = {estudiante1, estudiante2, estudiante3};
+
+        for(int i=0; i<listado.length; i++){
+            System.out.println("Nombre: " + listado[i].getNombre());
+            System.out.println("Apellido: " + listado[i].getApellido());
+            System.out.println("Edad: " + listado[i].getEdad());
+            System.out.println("Carrera: " + listado[i].getCarrera());
+            System.out.println("Promedio: " + listado[i].getPromedio());
+            System.out.println("");
+        }
     }
 }
