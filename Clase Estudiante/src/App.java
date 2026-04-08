545a49983050a31458ad3b08d8670accc3915002
paicodev <paedmaag@gmail.com>
2026-04-08 19:17:04 -0300

    dev: Se instanciaron los 3 primeros objetos de tipo Estudiante

--- Clase Estudiante/src/App.java
+++ Clase Estudiante/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        Estudiante estudiante1 = new Estudiante("Pablo", "Lopez", 23, "Programador Universitario en Sistemas", 9.1); 
+        Estudiante estudiante2 = new Estudiante("Fernando", "Torres", 23, "Programador Universitario en Sistemas", 8.1); 
+        Estudiante estudiante3 = new Estudiante("Esteban", "Mendez", 23, "Programador Universitario en Sistemas", 8.7);
         
         
     }
